be00904cb3d8fbc6dbf221ec0b991195a61fcfdc
Sampath Bodakunti <sampathbodakunti@gmail.com>
2025-10-20 23:15:40 +0530

    Initial commit with Java code

---
+++ DataTypes.java
@@ -0,0 +1,41 @@
+package com.corejava.practice;
+
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+//		To find the size of each data type
+		System.out.println(Short.SIZE/8);
+		System.out.println(Integer.SIZE/8);
+		System.out.println(Long.SIZE/8);
+		System.out.println(Character.SIZE/8);
+System.out.println("---------------------------------------------");
+		
+//		To find min and max capacity of data types that can store numericals
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Character.MIN_VALUE);
+		System.out.println(Character.MAX_VALUE);
+		
+
+		
+		byte a=10;
+		int b=20;
+		long c=30;
+		double d=40;
+		char e='s';
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+	}
+
+}
