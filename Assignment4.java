0e5de6b1c3e15e5d5842ee658029a3f780adcdb9
Sampath Bodakunti <sampathbodakunti@gmail.com>
2025-11-11 21:19:25 +0530

    printing 1-100 numbers using while and Do_While loop

---
+++ Assignment4.java
@@ -0,0 +1,19 @@
+package com.corejava.practice;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=100) {
+			System.out.println(i);
+			i++;
+		}
+		
+		int j=1;
+		do {
+			System.out.println(j);
+			j++;
+		} while (j<=100);
+	}
+
+}
