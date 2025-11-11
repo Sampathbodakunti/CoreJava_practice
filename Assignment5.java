19cf21db3017278f84fd958ba6e32d92c85b4bd3
Sampath Bodakunti <sampathbodakunti@gmail.com>
2025-11-11 21:29:00 +0530

    printing even numbers btw 200-500 while and dowhile

---
+++ Assignment5.java
@@ -0,0 +1,15 @@
+//Printing even numbers form 200 to 500
+package com.corejava.practice;
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		int i=499;
+		do {
+			if(i%2==0) {
+			System.out.println(i);
+			}i++;
+			
+		}while(i<=500);
+	}
+
+}
