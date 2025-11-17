d8cdd7804258f259fe9b84b1a63368522c942ae9
Sampath Bodakunti <sampathbodakunti@gmail.com>
2025-11-17 23:18:57 +0530

    Day12-single-D array

---
+++ DemoArray.java
@@ -0,0 +1,16 @@
+package com.corejava.practice;
+
+public class DemoArray {
+
+	public static void main(String[] args) {
+		int[] i= new int[5];
+		int j[];
+		i[1]=12;
+		i[0]=13;
+		
+		for(int x=0;x<i.length;x++) {
+			System.out.println(i[x]);
+		}
+	}
+
+}
