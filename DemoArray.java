5de04e2cb51906efe2e37c0a510d207f6f69dac7
Sampath Bodakunti <sampathbodakunti@gmail.com>
2025-11-17 23:01:32 +0530

    Single-D array

---
+++ DemoArray.java
@@ -0,0 +1,12 @@
+//demonstration for single dimensional array
+package com.corejava.practice;
+public class DemoArray {
+
+	public static void main(String[] args) {
+		int i[]=new int[5];
+		int[] j;
+	   i[0]=10;
+	   for (int x = 0; x < i.length; x++) {
+           System.out.println(i[x]);
+       }	   }
+}
